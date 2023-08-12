d8f798f34188a333e447b7d8097ac9bd13ec300c
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2023-08-12 23:00:37 +0700

    fixed some errors, added image hashes

---
+++ pantheon.lua
@@ -0,0 +1,49 @@
+local data = {	{
+		id = "TheBrineKing", 
+		is_major_god = true, 
+	},
+	{
+		id = "Arakaali", 
+		is_major_god = true, 
+	},
+	{
+		id = "Solaris", 
+		is_major_god = true, 
+	},
+	{
+		id = "Lunaris", 
+		is_major_god = true, 
+	},
+	{
+		id = "Abberath", 
+		is_major_god = false, 
+	},
+	{
+		id = "Gruthkul", 
+		is_major_god = false, 
+	},
+	{
+		id = "Yugul", 
+		is_major_god = false, 
+	},
+	{
+		id = "Shakari", 
+		is_major_god = false, 
+	},
+	{
+		id = "Tukohama", 
+		is_major_god = false, 
+	},
+	{
+		id = "Ralakesh", 
+		is_major_god = false, 
+	},
+	{
+		id = "Garukhan", 
+		is_major_god = false, 
+	},
+	{
+		id = "Ryslatha", 
+		is_major_god = false, 
+	}}
+return data
